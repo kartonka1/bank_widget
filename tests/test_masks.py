c5a1e247c1a4de25fbd9d57dc5f5b6be07b7d97b
Kartonka1 <0663340636S@gmail.com>
2025-10-30 21:41:25 +0300

    Code formatting and linting cleanup

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,13 @@
-from src.masks import get_mask_card_number, get_mask_account
 import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def test_get_mask_card_number():
     assert get_mask_card_number(1234567812345678) == "1234 56** **** 5678"
     with pytest.raises(ValueError):
         get_mask_card_number(123)  # слишком короткий номер
+
 
 def test_get_mask_account():
     assert get_mask_account(9876543210) == "**3210"
